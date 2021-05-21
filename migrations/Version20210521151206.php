4f9e7e3666d1af285d4f40f01fd46dc6b14089b8
Mikhaïl Uchiha D <kanon_des_gemeaux@msn.com>
2021-05-21 17:12:25 +0200

    test heroku

--- migrations/Version20210521151206.php
+++ migrations/Version20210521151206.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210521150237 extends AbstractMigration
+final class Version20210521151206 extends AbstractMigration
 {
     public function getDescription(): string
     {
